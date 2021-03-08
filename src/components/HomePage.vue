979479dc268577c6b220442f45cf9e68858e8259
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-03-08 23:15:42 +0100

    Updates

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,15 @@
 <template>
   <v-container>
+    <!-- <v-row>
+      <v-col cols="12">
+        <v-input
+          append-icon="mdi-close"
+          prepend-icon="mdi-magnify"
+          label="Search Here"
+        >
+        </v-input>
+      </v-col>
+    </v-row> -->
     <v-layout row wrap class="mt-4">
       <v-flex xs12>
         <v-carousel style="cursor: pointer">
@@ -14,12 +24,17 @@
         </v-carousel>
       </v-flex>
     </v-layout>
-    <div class="container my-12 mx-auto px-4 md:px-12">
+    <div
+      class="container my-12 mx-auto px-4 md:px-12"
+      v-for="merchant in merchants"
+      :key="merchant"
+    >
+      <div class="text-xl font-bold">{{ merchant.name }}</div>
       <div class="flex flex-wrap -mx-1 lg:-mx-4">
         <div
           class="my-1 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/4"
-          v-for="article in articles"
-          :key="article"
+          v-for="product in merchant.products"
+          :key="product"
         >
           <article class="overflow-hidden rounded-lg shadow-lg">
             <a href="/">
@@ -34,8 +49,12 @@
               class="flex items-center justify-center leading-tight p-2 md:p-4"
             >
               <h1 class="text-lg text-center">
-                <a class="no-underline hover:underline text-grey-700" style="color:black;" href="#">
-                  Title Goes Here
+                <a
+                  class="no-underline hover:underline text-grey-700"
+                  style="color: black"
+                  href="#"
+                >
+                  {{ product }}
                 </a>
               </h1>
             </header>
@@ -54,11 +73,46 @@ export default {
       return [
         "https://images.pexels.com/photos/1447092/pexels-photo-1447092.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         "https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+        "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
       ];
     },
-    articles() {
-      return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    merchants() {
+      return [
+        {
+          name: "Product Merchants",
+          products: [
+            "Product 1",
+            "Product 2",
+            "Product 3",
+            "Product 4",
+            "Product 5",
+            "Product 6",
+            "Product 7",
+            "Product 8",
+            "Product 9",
+            "Product 10",
+            "Product 11",
+            "Product 12",
+          ],
+        },
+        {
+          name: "Service Merchants",
+          products: [
+            "Service 1",
+            "Service 2",
+            "Service 3",
+            "Service 4",
+            "Service 5",
+            "Service 6",
+            "Service 7",
+            "Service 8",
+            "Service 9",
+            "Service 10",
+            "Service 11",
+            "Service 12",
+          ],
+        },
+      ];
     },
   },
 };
